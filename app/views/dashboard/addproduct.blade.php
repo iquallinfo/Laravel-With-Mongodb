7882e5c3fbe7bc3f3780c41267dae4ef94276e83
yasin sama <work@i-quall.com>
2019-03-02 15:48:45 +0530

    Update addproduct.blade.php

--- app/views/dashboard/addproduct.blade.php
+++ app/views/dashboard/addproduct.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 @section('content')
 
-<!-- Services Section -->
+<!-- Add Product Section -->
 <!-- ============================= Content Area ========================================== -->        
 
 <div class="content dashboard_page">
